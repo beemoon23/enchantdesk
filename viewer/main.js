defef2663c27ffa2764606a4197035c946c3cfe2
beemoon23 <89347105+beemoon23@users.noreply.github.com>
2026-09-06 15:37:59 -0300

    Remove clipboard monitoring from main.js
    Removed clipboard functionality and related variables.

--- viewer/main.js
+++ viewer/main.js
@@ -1,8 +1,4 @@
-const { app, BrowserWindow, clipboard } = require('electron');
-
-let janelaGlobal = null;
-let ultimoClipboardViewer = '';
-let ignorarProximaLeituraClipboard = false;
+const { app, BrowserWindow } = require('electron');
 
 function criarJanela() {
   const janela = new BrowserWindow({
@@ -15,23 +11,8 @@ function criarJanela() {
     }
   });
 
-  janelaGlobal = janela;
   janela.loadURL('http://10.0.0.52:3000');
   janela.setMenuBarVisibility(false);
-
-  setInterval(() => {
-    const atual = clipboard.readText();
-    if (atual !== ultimoClipboardViewer) {
-      ultimoClipboardViewer = atual;
-      if (ignorarProximaLeituraClipboard) {
-        ignorarProximaLeituraClipboard = false;
-        return;
-      }
-      if (atual && atual.length < 100000) {
-        janela.webContents.send('clipboard-mudou', atual);
-      }
-    }
-  }, 1000);
 }
 
 app.whenReady().then(criarJanela);
